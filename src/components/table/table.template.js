cf83acbb82c21985560695394432fbfd334e48cd
Nodir Navruzov <spy.nowin@gmail.com>
2020-10-28 09:49:51 +0500

    Finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -2,22 +2,29 @@ const CODES = {
   A: 65,
   Z: 90,
 }
-function toCell() {
+function toCell(_, col) {
   return `
-    <div class="column" contenteditable></div>
+    <div class="column" contenteditable data-col="${col}"></div>
     `
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column">${col}</div>
+    <div class="column" data-type="resizable" data-col="${index}">
+    ${col}
+    <div class="col-resize" data-resize="col"></div>
+    </div>
     `
 }
 
 function createRow(index, content) {
+  const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
   return `
-    <div class="row">
-    <div class="row-info">${index ? index : ''}</div>
+    <div class="row" data-type="resizable">
+    <div class="row-info">
+    ${index ? index : ''}
+    ${resize}
+    </div>
     <div class="row-data">${content}</div>
     </div>
     `
@@ -30,11 +37,22 @@ function toChar(_, index) {
 export function createTable(rowsCount = 15) {
   const colsCount = CODES.Z - CODES.A + 1
   const rows = []
-  const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('')
+  // prettier-ignore
+  const cols = new Array(colsCount)
+      .fill('')
+      .map(toChar)
+      .map(toColumn)
+      .join('')
+
   rows.push(createRow(null, cols))
 
   for (let i = 0; i < rowsCount; i++) {
-    const cells = new Array(colsCount).fill('').map(toCell).join('')
+    // prettier-ignore
+    const cells = new Array(colsCount)
+        .fill('')
+        .map(toCell)
+        .join('')
+
     rows.push(createRow(i + 1, cells))
   }
   return rows.join('')
